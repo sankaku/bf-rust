f1be98cd8cf17a0f70c65f86dd294580e93ec26c
sankaku <sankaku.git@gmail.com>
2020-05-31 19:02:00 +0900

    read input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args().nth(1).expect(&usage());
+
+    let content = fs::read_to_string(filename);
+    match content {
+        Result::Ok(val) => println!("{:?}", val),
+        Result::Err(err) => println!("[ERROR] {:?}", err),
+    }
+}
+
+fn usage() -> String {
+    String::from("cargo run <FILENAME>")
 }
